c8c48dec12f49c71d5c5a6356a4ce6141221ea09
ApolloArcadia <hutchisoncm@vcu.edu>
2017-02-25 13:31:34 -0500

    <scanner>

--- FileSearch.java
+++ FileSearch.java
@@ -9,11 +9,12 @@
  *
  * Pledge: I have neither given nor received unauthorized aid on the program.
  *
- * Description: Create a program to approximate how many jelly beans are in a jar
+ * Description: Create a program to ask for a name of a file, open said file, find a search term in said file
+ * ,and print out the strings that contain the search term
  *
- * Inputs: Equatrial Diameter (d), Polar Diameter (l), Jar Size (s)
+ * Inputs: File Name (fileName), Search Term(userInput)
  *
- * Output: "There are approximately (#ofjellybeans) beans in the jar"
+ * Output: Strings that contain the user input for Search term in the named document they input
  *
  ***************************************************************************/
 import java.util.Scanner;
@@ -21,30 +22,39 @@ import java.io.*;
 public class FileSearch
 {
   public static void main(String[] args) throws FileNotFoundException{
+    Scanner scan = new Scanner(System.in);
     /*
-     * User Input of file name and search term
+     * User Input of File Name
      */
     System.out.println("Enter the name of the file ");
-    Scanner fileName = new Scanner(System.in);
-    
-    Scanner keyboard = new Scanner(System.in);
-    
-    String filename = keyboard.nextLine();
-    File file = new File(filename);
+    String fileName = scan.nextLine();
+    /*
+     * Opening the File
+     */ 
+    File file = new File(fileName);
     Scanner inputFile = new Scanner(file);
-    
+    /*
+     * User Input of Search Term
+     */ 
     System.out.println("Enter the search term");
-    Scanner userInput = new Scanner(System.in);
+    String userInput = scan.next();
   
-      
+    /*
+     * While loop that Reads document, find the strings that contain the search term, and prints them
+     */ 
     while(inputFile.hasNext()){
          String line = inputFile.nextLine();
          if(line.contains(userInput)){
              System.out.println(line);
          }
     }
-
-    System.out.println();
+    
+    /*
+     * Closing the Scanner variables
+     */ 
+    scan.close();
+    inputFile.close();
+    
   }
 }
     
